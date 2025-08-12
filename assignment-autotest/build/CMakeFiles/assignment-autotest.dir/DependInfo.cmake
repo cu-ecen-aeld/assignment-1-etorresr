0b55173271e84989a4b7dc3c61d6bcf4d14f010c
etorresr <49214324+etorresr@users.noreply.github.com>
2025-08-12 11:51:56 -0600

    Add files via upload

---
+++ assignment-autotest/build/CMakeFiles/assignment-autotest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/emmanuel-torres/Documents/cursera/linux_embed/assignment-1-etorresr/assignment-autotest/test/unity_runner.c" "CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o" "gcc" "CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d"
+  "" "assignment-autotest" "gcc" "CMakeFiles/assignment-autotest.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assignment-autotest/build/CMakeFiles/assignment-autotest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment-autotest.dir/link.d"
+  "/home/emmanuel-torres/Documents/cursera/linux_embed/assignment-1-etorresr/assignment-autotest/test/unity_runner.c"
+  "CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o"
+  "CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d"
+  "assignment-autotest"
+  "assignment-autotest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment-autotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
